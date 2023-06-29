ba032b0bb3602782e0b2acda915576800809d03b
Ahmed Yusuf <yusufbabatundeahmed@gmail.com>
2023-06-29 13:27:31 +0100

    Update Answer to Question3.sql

--- Answer to Question3.sql
+++ Answer to Question3.sql
@@ -1,3 +1,5 @@
+#•	In the same table, return which particular trip has longest duration and the trip that has the shortest duration (return all the information(columns) on the table for this record) If more than 1 record has the same duration, return the earliest trip [start time]
+
 SELECT TOP 2 *
 FROM [dbo].[BikerDatav2]
 WHERE end_station_name NOT IN ('Missing', 'Stolen')
