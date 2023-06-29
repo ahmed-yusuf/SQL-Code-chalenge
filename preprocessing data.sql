c88ec098a0211f296a6c02baa4650bcc424700c8
Ahmed Yusuf <yusufbabatundeahmed@gmail.com>
2023-06-29 13:22:29 +0100

    Update preprocessing data.sql

--- preprocessing data.sql
+++ preprocessing data.sql
@@ -1,6 +1,10 @@
+#Replace the utc in the starttime column to be able to convert into an appropriate date format
+
 UPDATE [dbo].[BikerDatav2]
 SET  [dbo].[BikerDatav2].start_time= REPLACE([dbo].[BikerDatav2].start_time, ' UTC', '');
 SELECT * FROM [dbo].[BikerDatav2]
 
+
+#convertinh column into date format for better analysis
 UPDATE [dbo].[BikerDatav2]
 SET  [dbo].[BikerDatav2].start_time= CONVERT(DATETIME, [start_time], 120);;
