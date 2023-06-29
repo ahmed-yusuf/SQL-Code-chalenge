7aaf268c7a552dd37304f546bfae96e938b5d1ef
Ahmed Yusuf <yusufbabatundeahmed@gmail.com>
2023-06-29 13:26:43 +0100

    Update Answer to Question2.sql

--- Answer to Question2.sql
+++ Answer to Question2.sql
@@ -1,3 +1,4 @@
+#Question2:What month/year has the most bike trips and what is the count of the trips?
 SELECT TOP 1 DATEPART(MONTH, start_time) AS month, DATEPART(YEAR, start_time) AS year, COUNT(*) AS trip_count
 FROM [dbo].[BikerDatav2]
 GROUP BY DATEPART(MONTH, start_time), DATEPART(YEAR, start_time)
