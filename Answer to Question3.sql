4790fa16b5ceb354bb7ead820c4301d6bc597209
Ahmed Yusuf <yusufbabatundeahmed@gmail.com>
2023-06-29 13:28:30 +0100

    Update Answer to Question3.sql

--- Answer to Question3.sql
+++ Answer to Question3.sql
@@ -1,4 +1,4 @@
-#•	In the same table, return which particular trip has longest duration and the trip that has the shortest duration (return all the information(columns) on the table for this record) If more than 1 record has the same duration, return the earliest trip [start time]
+--	In the same table, return which particular trip has longest duration and the trip that has the shortest duration (return all the information(columns) on the table for this record) If more than 1 record has the same duration, return the earliest trip [start time]
 
 SELECT TOP 2 *
 FROM [dbo].[BikerDatav2]
