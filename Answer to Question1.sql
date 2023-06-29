499b2a486647b0cf45261f8d74714003507c7cfb
Ahmed Yusuf <yusufbabatundeahmed@gmail.com>
2023-06-29 13:25:58 +0100

    Update Answer to Question1.sql

--- Answer to Question1.sql
+++ Answer to Question1.sql
@@ -1,3 +1,4 @@
+#Question1:On which day of the week do we on average have the longest trip?
 SELECT TOP 1 DATEPART(WEEKDAY, start_time) AS day_of_week, AVG(duration_minutes) AS average_duration
 FROM BikerDatav2
 GROUP BY DATEPART(WEEKDAY, start_time)
